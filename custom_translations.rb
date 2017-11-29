3ad24b269dd19b84e94d051bbe903e50fa05dc62
anne <richardson.ae@gmail.com>
2017-11-28 21:09:26 -0600

    Sets up blank doc with questions

---
+++ custom_translations.rb
@@ -0,0 +1,5 @@
+# Come up with 3 queries just complex enough to test your understanding.
+# At least one should use an aggregate function (e.g. COUNT)
+# and at least one should us GROUP. Don't repeat the examples we've
+# just used above. Then write down each of those queries in both SQL
+# and ActiveRecord.
