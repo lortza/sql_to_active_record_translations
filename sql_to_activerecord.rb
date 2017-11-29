9090150e1962cf99404683945070f631543eb346
anne <richardson.ae@gmail.com>
2017-11-28 21:26:47 -0600

    Reformats for clarity

--- sql_to_activerecord.rb
+++ sql_to_activerecord.rb
@@ -1,23 +1,36 @@
-# 1. SELECT * FROM users;
+# 1.
+SELECT *
+  FROM users;
+
 users = User.all
 
-# 2. SELECT *
-# FROM users
-# WHERE user.id = 1;
+
+# 2.
+SELECT *
+  FROM users
+  WHERE user.id = 1;
+
 users = User.find(1)
 
-# 3. SELECT *
-# FROM posts
-# ORDER BY created_at DESC
-# LIMIT 1;
+
+# 3.
+SELECT *
+  FROM posts
+  ORDER BY created_at DESC
+  LIMIT 1;
+
 posts = Post.order(created_at: :desc)
 
-# 4. SELECT *
-# FROM users
-# JOIN posts
-# ON posts.author_id = users.id
-# WHERE posts.created_at >= '2014-08-31 00:00:00';
+
+# 4.
+SELECT *
+  FROM users
+  JOIN posts
+  ON posts.author_id = users.id
+  WHERE posts.created_at >= '2014-08-31 00:00:00';
+
 User.joins(:posts).where("posts.created_at >= '2014-08-31 00:00:00'")
+
 
 # ---- this SQL works in the rails db
 # 5.
@@ -39,11 +52,14 @@ User.select('COUNT(*) AS usr_count').group(:name).having('usr_count >= 3')
 # 6. The most recently updated user
 User.order(updated_at: :desc).limit(1)
 
+
 # 7. The oldest user (by age)
 User.order(age: :desc).limit(1)
 
+
 # 8. all the users
 User.all
+
 
 # 9. all posts sorted in descending order by date created
 Post.order(created_at: :desc)
