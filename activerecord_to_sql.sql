120ee2020ac395162ad5a33a262a25ae27d8ea17
anne <richardson.ae@gmail.com>
2017-11-28 21:02:07 -0600

    Adds questions to blank file

--- activerecord_to_sql.sql
+++ activerecord_to_sql.sql
@@ -1,1 +1,23 @@
-activerecord_to_sql.sql
+-- 1. Post.all
+
+-- 2. Post.first
+
+-- 3. Post.last
+
+-- 4. Post.where(:id => 4)
+
+-- 5. Post.find(4)
+
+-- 6. User.count
+
+-- 7. Post.select(:name).where(:created_at > 3.days.ago).order(:created_at)
+
+-- 8. Post.select("COUNT(*)").group(:category_id)
+
+-- 9. All posts created before 2014
+
+-- 10. A list of all (unique) first names for authors who have written at least 3 posts
+
+-- 11. The posts with titles that start with the word "The"
+
+-- 12. Posts with IDs of 3,5,7, and 9
