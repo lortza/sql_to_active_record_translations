7df216f502beac4eacea687df70c5d5cd83ad2f7
anne <richardson.ae@gmail.com>
2017-11-28 21:42:21 -0600

    Answers 1-7

--- activerecord_to_sql.sql
+++ activerecord_to_sql.sql
@@ -1,18 +1,49 @@
 -- 1. Post.all
+SELECT *
+  FROM posts;
+
 
 -- 2. Post.first
+SELECT *
+  FROM posts p
+  ORDER BY p.id ASC
+  LIMIT 1;
+
 
 -- 3. Post.last
+SELECT *
+  FROM posts p
+  ORDER BY p.id DESC
+  LIMIT 1;
+
 
 -- 4. Post.where(:id => 4)
+SELECT *
+  FROM posts p
+  WHERE p.id = 4;
+
 
 -- 5. Post.find(4)
+SELECT *
+  FROM posts p
+  WHERE p.id = 4
+  LIMIT 1;
+
 
 -- 6. User.count
+SELECT COUNT(*)
+  FROM users;
 
--- 7. Post.select(:name).where(:created_at > 3.days.ago).order(:created_at)
+
+-- 7. Post.select(:title).where(:created_at > 3.days.ago).order(:created_at)
+SELECT p.title
+  FROM posts p
+  WHERE p.created_at > CURRENT_DATE - 3
+  ORDER BY p.created_at;
+
 
 -- 8. Post.select("COUNT(*)").group(:category_id)
+
 
 -- 9. All posts created before 2014
 
